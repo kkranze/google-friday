d35aa8b16093a32458e4ef8e80e0fc7cb250c1d9
kkranze <kkranze@sjnma.org>
2017-09-18 13:35:28 -0500

    primitive variables

---
+++ hello.py
@@ -0,0 +1,6 @@
+name=input("please enter your name")
+teacher = "mr. gold"
+print("hello world")
+print(name)
+
+print(name + "is a great student "+ teacher.upper() + " tends to agree")
---
+++ math.py
@@ -0,0 +1,4 @@
+x=20+35
+print(x)
+y=3*x+40
+print(y)
